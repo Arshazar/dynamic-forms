6a27f777a844ecd6464af20c101a0240aceecf91
Arshavir Azarfar <4rsh4vir4z4rf4r@gmail.com>
2023-06-24 16:53:48 +0330

    ThemeProvider added

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { ThemeProvider, defaultTheme } from 'evergreen-ui'
 
-import App from './App.tsx'
-import './styles/globals.css'
+import App from './App'
+import './styles/global.css'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider value={defaultTheme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 )
